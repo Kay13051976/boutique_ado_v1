43b2bce145a0a541031249de30716588cd4fafb2
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-02-25 20:58:45 +0000

    Updated checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,6 +6,7 @@ from products.models import Product
 import json
 import time
 
+
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -63,7 +64,8 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=f'Webhook received: {
+                    event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -106,7 +108,8 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {
+                event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
